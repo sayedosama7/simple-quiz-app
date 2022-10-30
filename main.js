0fe61b02fea3df131a0e38002739d1eaa5991e1c
sayedosama7 <sayedosama088@gmail.com>
2022-10-30 21:58:03 +0200

    simple quiz app

---
+++ main.js
@@ -0,0 +1,19 @@
+function result(){
+    var score =0;
+    if(document.getElementById("correct1").checked){
+        score++;
+    }
+    if(document.getElementById("correct2").checked){
+        score ++;
+    }   
+    if(document.getElementById("correct3").checked){
+        score ++;
+    }    
+    if(document.getElementById("correct4").checked){
+        score ++;
+    }
+    if(document.getElementById("correct5").checked){
+        score ++;
+    }
+    alert("your score is " +score);
+}
